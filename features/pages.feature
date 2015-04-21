c7ecc1e42a1b3b4dd73d02c234e342a56cb76e8b
Jason Preston <jason@prestons.me>
2015-04-21 16:52:01 -0700

    added some tests for signing up and logging in

--- features/pages.feature
+++ features/pages.feature
@@ -1,14 +1,25 @@
 Feature: Pages
 	
-Scenario: Viewing Raisin's home page
+Scenario: Viewing Raisin's home page without logging in
 	When I go to the home page
 	Then I should see "most important startup"
-	And I should see "Projects"
 	And I should see "Copyright (c) 2015"
 	And I should see "Log in"
 
 @signup
-Scenario: Viewing Raisin's log-in page
-	When I go to the log in page
-	Then I should see "Log in"
+Scenario: Viewing Raisin's sign up page
+	When I go to the sign up page
+	Then I should see "Sign up"
 	And I should see "Sign in with Facebook"
+	
+@signup
+Scenario: A user signs up
+	When a user signs up
+	Then I should see "You have signed up successfully."
+	
+@signin	
+Scenario: A user signs in
+	Given the user has an account
+	When she logs in
+	Then I should see "Signed in successfully."
+	
--- features/step_definitions/pages_steps.rb
+++ features/step_definitions/pages_steps.rb
@@ -2,10 +2,29 @@ When(/^I go to the home page$/) do
   visit(root_path)
 end
 
-When(/^I go to the log in page$/) do 
-  visit(new_user_session_path)
+When(/^I go to the sign up page$/) do 
+  visit(new_user_registration_path)
 end
 
 Then(/^I should see "(.*?)"$/) do |arg1|
   page.should have_content(arg1)
+end
+
+When /^a user signs up$/ do
+  visit(new_user_registration_path)
+  fill_in('Email', :with => "example@example.com")
+  fill_in('Password', :with => "godzilla")
+  fill_in('Password confirmation', :with => "godzilla")
+  click_button('Sign up')
+end
+
+Given /^the user has an account$/ do
+  @user = User.create!(:email => "example@example.com", :password => "godzilla", :password_confirmation => "godzilla")  
+end
+
+When /^she logs in$/ do
+  visit(new_user_session_path)
+  fill_in('Email', :with => "example@example.com")
+  fill_in('Password', :with => "godzilla")
+  click_button('Log in')
 end
