4103ecf4f59a818bc45fdd815fa21a7da1bb0895
Jason Preston <jason@prestons.me>
2015-04-21 22:54:31 -0700

    tried removing FB javascript to see if that was the error on Heroku

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -5,7 +5,8 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect new_account_path
+      sign_in(@user) 
+      redirect_to new_account_path
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
