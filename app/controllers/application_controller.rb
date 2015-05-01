524ebb572b494f05b2e72e85bc956ca24cd17cae
Jason Preston <jason@prestons.me>
2015-05-01 08:09:41 -0700

    made tasks dashboard with ajax date filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,22 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  helper_method :return_custom_potential_users_to_assign
+
+  def return_custom_potential_users_to_assign(project)
+    #[{"id"=>1, "name"=>"Jason Preston"}]
+    members = Member.where.not(user_id: nil).where(project_id: project.id)
+    user_names_and_ids_hash = Hash.new
+    user_names_and_ids_hash["Assign it"] = 0
+    members.each do |member|
+      name = member.user.name 
+      id = member.user.id
+      user_names_and_ids_hash[name] = id
+    end
+
+    return user_names_and_ids_hash
+  end
 
   protected
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,33 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find_by_id(params[:id])
+    @account = Account.find_by_id(session["preferred_account_id"]) if session["preferred_account_id"]
+    
+    # Let's set the variables to allow task viewing
+    @date_options = ["Everything", "This week", "Today", "Tomorrow", "Overdue", "In the future"]
+    @items = Item.where(user_id: @user.id, status: "active").order(:list_id)
+    
+    if params[:date_range]
+      if params[:date_range] == "Today"
+        @items = Item.where(user_id: @user.id, status: "active", due: Time.now.utc.midnight..(Time.now.utc.midnight + 1.day - 1.minute) ).order(:list_id)
+      elsif params[:date_range] == "Overdue"
+        @items = Item.where(user_id: @user.id, status: "active", due: (Time.now.utc.midnight - 1.month )..(Time.now.utc.midnight - 1.minute) ).order(:list_id)
+      elsif params[:date_range] == "Tomorrow"
+        @items = Item.where(user_id: @user.id, status: "active", due: Time.now.utc.midnight.tomorrow).order(:list_id)   
+      elsif params[:date_range] == "This week"
+        @items = Item.where(user_id: @user.id, status: "active", due: (Time.now.utc.midnight.beginning_of_week(:sunday))..(Time.now.utc.midnight.end_of_week(:sunday)) ).order(:list_id)   
+      elsif params[:date_range] == "In the future"
+        @items = Item.where(user_id: @user.id, status: "active", due: (Time.now.utc.midnight.tomorrow)..(Time.now.utc.midnight + 1.month) ).order(:list_id)                  
+      elsif params[:date_range] == "Everything"
+        @items = Item.where(user_id: @user.id, status: "active").order(:list_id)    
+      end           
+    end
+    
+    respond_to do |format|
+      format.html { render 'show' }
+      format.js { render 'show' }
+    end
+    
   end
   
   def edit
