684052899ff95b8524f30fa0c62adf3bddff7427
Jason Preston <jason@prestons.me>
2015-05-05 16:12:50 -0700

    trying to associate incoming comments

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -16,7 +16,19 @@ class EmailProcessor
     
     user = User.find_by_email(@email.from[:email])
     if user
-      comment = Comment.create!(content: @email.body, user_id: user.id)
+      id_string = @email.to
+      string = id_string.split("-")
+      
+      # do some "introspection"
+      if string.include?("comment")
+        # it's a comment reply, item & comment ID supplied
+        item = Item.find(string[1])
+      else
+        # only two options right now: it's a reply to a to-do action: assigned, or completed
+        item = Item.find(string[1])
+      end
+      
+      comment = Comment.create!(content: @email.body, user_id: user.id, item_id: item.id)
     end
     
   end
