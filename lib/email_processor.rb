328915ea25f46e04cfa2bc02334fc31d7599edc7
Jason Preston <jason@prestons.me>
2015-05-05 21:07:28 -0700

    debug email thing

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -22,13 +22,18 @@ class EmailProcessor
       # do some "introspection"
       if string.include?("comment")
         # it's a comment reply, item & comment ID supplied
-        item = Item.find(string[1])
+        item = Item.find_by_id(string[1])
+        logger.info(item)
       else
         # only two options right now: it's a reply to a to-do action: assigned, or completed
-        item = Item.find(string[1])
+        logger.info(string[1])
+        item = Item.find_by_id(string[1])
       end
+      if item.id
+        item_id = item.id
+      end  
       
-      comment = Comment.create!(content: @email.body, user_id: user.id, item_id: item.id)
+      comment = Comment.create!(content: @email.body, user_id: user.id, item_id: item_id)
     end
     
   end
