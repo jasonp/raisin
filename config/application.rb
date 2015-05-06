484afd7a714b13e15110173aef96c7a2f3cbae24
Jason Preston <jason@prestons.me>
2015-05-05 22:49:56 -0700

    comment replies MIGHT work

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module Raisin
     config.active_record.raise_in_transactional_callbacks = true
     
     config.autoload_paths << Rails.root.join('lib')
+    config.autoload_paths += %W(#{config.root}/lib/email_processor.rb)
   end
 end
--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -1,4 +1,5 @@
 class EmailProcessor
+  
   def initialize(email)
     @email = email
   end
@@ -14,26 +15,26 @@ class EmailProcessor
     #  body: @email.body
     #)
     
-    user = User.find_by_email(@email.from[:email])
-    if user
-      id_string = @email.to
+    author = User.find_by_email(@email.from[:email])
+    
+    #author = User.where( email: @email.from ).first
+    if author
+      id_string = @email.to.first[:token]
       string = id_string.split("-")
       
       # do some "introspection"
       if string.include?("comment")
         # it's a comment reply, item & comment ID supplied
         item = Item.find_by_id(string[1])
-        logger.info(item)
       else
         # only two options right now: it's a reply to a to-do action: assigned, or completed
-        logger.info(string[1])
         item = Item.find_by_id(string[1])
       end
       if item.id
         item_id = item.id
       end  
       
-      comment = Comment.create!(content: @email.body, user_id: user.id, item_id: item_id)
+      comment = Comment.create!(content: @email.body, user_id: author.id, item_id: item_id)
     end
     
   end
