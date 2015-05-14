81626502d8f44b568e4c5d2d75f565994be3c0af
Jason Preston <jason@prestons.me>
2015-05-14 02:05:16 -0700

    trying to set env variable in HEROKU CLI - do not have high hopes

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,1 @@
 
-if Rails.env == 'production'
-  uri = URI.parse("redis://rediscloud:jdMbOCF90FOTAnml@pub-redis-15632.us-east-1-3.3.ec2.garantiadata.com:15632")
-  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-end
