cc5d2a32abb52bd617a5e1f99744a1b53dcc42ae
Jason Preston <jason@prestons.me>
2015-05-14 02:01:24 -0700

    ugh

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,5 @@
-if ENV["REDISCLOUD_URL"]
-    $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
+
+if Rails.env == 'production'
+  uri = URI.parse("redis://rediscloud:jdMbOCF90FOTAnml@pub-redis-15632.us-east-1-3.3.ec2.garantiadata.com:15632")
+  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
